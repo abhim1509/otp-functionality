f4fff199a00ea4253fa3a279c55f2ab2e1081cc5
Abhi <makker.abhinav@gmail.com>
2025-01-09 23:50:35 +0530

    Added maximum otp functionality

--- src/controllers/otp.controller.js
+++ src/controllers/otp.controller.js
@@ -15,7 +15,7 @@ export const generateOTP = async (req, res) => {
       otp: generateSecureOTP(),
     });
 
-    res.status(201).json({ message: "OTP created successfully", user: newOtp });
+    res.status(201).json({ message: "OTP created successfully", data: newOtp });
   } catch (err) {
     res.status(500).json({ message: "Error creating OTP", error: err.message });
   }
@@ -29,15 +29,23 @@ export const verifyOTP = async (req, res) => {
       return res.status(400).json({ message: "Email and OTP are required" });
     }
 
-    const validOtp = await Otp.find({
+    const otpResponse = await Otp.find({
       email,
       otp,
       expiresAt: { $gte: new Date(new Date() - 30000) },
     });
 
-    if (!validOtp || !validOtp.length) {
-      return res.status(400).json({ message: "OTP not verified." });
+    if (!otpResponse || !otpResponse.length) {
+      return res.status(404).json({ message: "OTP not found" });
     }
+
+    otpResponse.maxAttempts += 1;
+
+    if (maxAttempts > 3) {
+      return res.status(429).json({ message: "Maximum attempts reached." });
+    }
+
+    await otpResponse.save();
 
     res.status(200).json({ message: "OTP verified successfully" });
   } catch (err) {
